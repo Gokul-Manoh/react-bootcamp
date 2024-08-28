86e3b256412e395433505486c6668a06648a80e7
M <a-mg4@science.regn.net>
2024-08-28 17:07:47 +0530

    removed unwanted styling

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 import "./LoginPage.css";
-import {
-  MDBContainer,
-  MDBRow,
-  MDBCol,
-  MDBInput,
-} from "mdb-react-ui-kit";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export function LoginPage({ onLogin }) {
@@ -17,45 +11,21 @@ export function LoginPage({ onLogin }) {
   };
 
   return (
-    <MDBContainer className="gradient-form">
-      <MDBRow>
-        <MDBCol md="5" className="mb-5">
-          <div className="d-flex justify-content-center flex-column ms-md-5">
-            <div className="text-center">
-              <img
-                src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                style={{ width: "185px" }}
-                alt="logo"
-              />
-              <h4 className="mt-1 mb-5 pb-1">EKart</h4>
-            </div>
-
-            <p>Please login to your account</p>
-
-            <MDBInput
-              wrapperClass="mb-4"
-              placeholder="Username/Email Address"
-              id="txtusername"
-              type="email"
-            />
-            <MDBInput
-              wrapperClass="mb-4"
-              placeholder="Password"
-              id="txtpassword"
-              type="password"
-            />
-
-            <div className="text-center pt-1 mb-5 pb-1">
-              <button
-                className="mb-4 w-100 gradient-custom-2"
-                onClick={handleLogin}
-              >
-                Sign in
-              </button>
-            </div>
-          </div>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <div>
+      <h3 className="mt-4">Login</h3>
+      <form>
+        <input type="email" placeholder="Username" />
+        <br/>
+        <br/>
+        <input type="password" placeholder="Password" />
+        <br/>
+        <br/>
+        <button
+          onClick={handleLogin}
+        >
+          Sign in
+        </button>
+      </form>
+    </div>
   );
 }
