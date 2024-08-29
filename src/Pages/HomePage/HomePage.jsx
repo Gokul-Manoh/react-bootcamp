534ade627340492b61d66a3d8a224711ea7fd437
GokulM-Elsevier <g.m2@elsevier.com>
2024-08-29 18:39:55 +0530

    Added hooks and api in homepage and created product description page

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,25 +1,43 @@
-import React from "react";
-import { Container, Col, Row } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
 import "./HomePage.css";
 
-export function HomePage({ handleLogout }) {
+export function HomePage() {
   // Define state to store the api data of products
   // Add code to make a call to the API to fetch the products and store it in the component https://fakestoreapi.com/products
 
-  const isLoading = false;
+  const [products, setproducts] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+
+  useEffect(() => {
+    axios.get('https://fakestoreapi.com/products')
+    .then((response) => {
+      setisLoading(true);
+      setproducts(response.data)
+      setisLoading(false);
+    })
+    .catch(err => console.log(err))
+  }, []);
+
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <h2>Loading...</h2>;
   }
 
   return (
-    <Container>
-      <Row>
-        <Col md={9} className="product-view-col">
-          <h2>Home page goes here</h2>
-
-          {/* Add code to show the API data on the home page */}
-        </Col>
-      </Row>
-    </Container>
+    <div style={{padding: "2rem"}}>
+      {/* Add code to show the API data on the home page */}
+      {
+        products?.map((product) => (
+          <div key={product.id} style={{cursor: "pointer"}}>
+            <div>
+            <p style={{fontWeight: "500"}}>{product.title}</p>
+            <p style={{fontWeight: "500", fontSize: "1.5rem", color: "green"}}>$ {product.price}</p>
+            </div>
+            <img src={product.image} height="75" width="75" />
+            <hr />
+          </div>
+        ))
+      }
+    </div>
   );
 }
--- src/Pages/ProductDescription/ProductDescription.jsx
+++ src/Pages/ProductDescription/ProductDescription.jsx
@@ -1,22 +1,39 @@
-import React from "react";
-import { Container } from "react-bootstrap";
+import React, {useEffect, useState} from "react";
+import axios from 'axios';
 import "./ProductDescription.css";
 
-export const ProductDescription = () => {
+export function ProductDescription() {
 
   // Add state to store single product
 
   // Add code to trigger api call to https://fakestoreapi.com/products/${id}
   // id is fetched from useParams()
+
+  const [product, setproduct] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+
+  useEffect(() => {
+    axios.get(`https://fakestoreapi.com/products/2`)
+    .then((response) => {
+      setisLoading(true);
+      setproduct(response.data)
+      setisLoading(false);
+    })
+    .catch(err => console.log(err))
+  }, []);
   
-  const isLoading = true
   if (isLoading) {
     return <div>Loading...</div>;
   }
 
   return (
-    <Container className="product-description mt-8">
-      Product description page goes here
-    </Container>
+    <div className="product-description mt-8">
+      <div>
+        <p style={{fontWeight: "500"}}>{product.title}</p>
+        <p style={{color: "gray"}}>{product.description}</p>
+        <p style={{fontWeight: "500", fontSize: "1.5rem", color: "green"}}>$ {product.price}</p>
+        </div>
+      <img src={product.image} height="150" width="150" />
+    </div>
   );
 };
